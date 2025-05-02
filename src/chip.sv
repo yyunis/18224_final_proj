55964a0b1346fd23345c66bca53b4f6ff669a26c
yyunis <eunicel@andrew.cmu.edu>
2025-05-02 17:47:24 -0400

    main code without task of fft

--- src/chip.sv
+++ src/chip.sv
@@ -6,43 +6,77 @@ module my_chip (
     input logic clock,
     input logic reset // Important: Reset is ACTIVE-HIGH
 );
-    
-    // Basic counter design as an example
-    // TODO: remove the counter design and use this module to insert your own design
-    // DO NOT change the I/O header of this design
+    logic buffer_full;
+    reg [15:0] buffer [0:15]; // array of 16 samples that are 16 bits wide
+    logic [3:0] buffer_index;
+    logic top_half, fft_done;
+    reg [15:0] real_num [0:15];
+    reg [15:0] imag [0:15];
+    reg [3:0] peak_bin;
 
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+    // INPUT: microcontroller sends samples from mic
+    // OUTPUT: output is the frequency bin --> calculation of peak frequency + MIDI note done on microcontroller
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
-
-    always @(posedge clock) begin
+    always @(posedge clock, posedge reset) begin
         // if reset, set counter to 0
         if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
+            buffer_index <= 0;
+            buffer_full <= 0;
+            top_half <= 1;
+            for (integer i = 0; i < 16; i = i + 1) begin
+                for (integer j = 0; j < 16; j = j + 1) begin
+                    buffer[i][j] <= 0;
+                end
+            end
         end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
-
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
+            // STEP 1: Buffer to collect samples from microcontroller
+            if (!buffer_full) begin
+                buffer[buffer_index] <= io_in[7:0];
+                if (!top_half) buffer_index <= buffer_index + 1;
+                else buffer[buffer_index] <= buffer[buffer_index] << 8;
+                if (buffer_index == 15) buffer_full <= 1;
+                top_half <= ~top_half;
+            end
+            if (buffer_full) begin
+                if (fft_done) begin
+                    // MAGNITUDE COMPUTATION
+                    // STEP 3: Magnitude of bins
+                    // Calculate magnitudes of each bin
+                    reg [31:0] best = 0;
+                    reg [3:0] best_bin = 0;
+                    reg[31:0] temp_mag = 0;
+                    for (integer i = 0; i < 16; i = i + 1) begin
+                        temp_mag = real_num[i]*real_num[i] + imag[i]*imag[i];
+                        if (temp_mag > best) begin
+                            best = temp_mag;
+                            best_bin = i;
+                        end
+                    end
+                    peak_bin <= best_bin;
+                    buffer_index <= 0;
+                    buffer_full <= 0;
+                    top_half <= 1;
+                    for (integer i = 0; i < 16; i = i + 1) begin
+                        for (integer j = 0; j < 16; j = j + 1) begin
+                            buffer[i][j] <= 0;
+                        end
+                    end
+                    fft_done <= 0;
+                    keep_output <= 1;
+                end
+                else begin
+                    keep_output <= 0;
+                    fft_task(buffer, real_num, imag);
+                end
+            end
         end
     end
 
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
+    // STEP 2: FFT --> conversation with ChatGPT about tasks and basic FFT
+    // Outputs the real and imaginary bins
+    task fft_task(input [15:0] buf [0:15], output [15:0] real_fft [0:15], output [15:0] imag_fft [0:15]);
+    endtask
+
+    assign io_out[3:0] = (keep_output) ? peak_bin : 0;
 
 endmodule
